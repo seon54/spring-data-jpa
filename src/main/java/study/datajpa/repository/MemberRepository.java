71d1b4d892d672572da11edc5d3510f29a317505
seon54 <seonhyeongpark54@gmail.com>
2020-12-27 00:54:05 +0900

    공통 인터페이스 적용

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    /**
+    /*
      * [구현체가 없는데 동작하는 이유]
      * 스프링이 MemberRepository 구현체를 만들어서 주입함
      */
