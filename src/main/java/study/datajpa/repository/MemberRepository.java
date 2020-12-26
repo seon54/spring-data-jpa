9bc8bb9720e7f95134fff6e05d081eea1650fcff
seon54 <seonhyeongpark54@gmail.com>
2020-12-27 00:48:30 +0900

    공통 인터페이스 설정

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /**
+     * [구현체가 없는데 동작하는 이유]
+     * 스프링이 MemberRepository 구현체를 만들어서 주입함
+     */
 }
