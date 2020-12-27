057ef660ee85f9975b7616b8ddc50e18f6d9d15b
seon54 <seonhyeongpark54@gmail.com>
2020-12-27 17:20:09 +0900

    메소드 이름으로 쿼리 생성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,10 +3,14 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     /*
      * [구현체가 없는데 동작하는 이유]
      * 스프링이 MemberRepository 구현체를 만들어서 주입함
      */
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
